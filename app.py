d2d9a464f03ebcc4acaa1c0f45d146c52400cddc
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-22 16:15:21 -0700

    test endponits

--- app.py
+++ app.py
@@ -22,16 +22,16 @@ snaptrade = SnapTrade(
 # with open("accounts.json", "w", encoding="utf-8") as f:
 #     json.dump(res.body, f, indent=2, default=str)
 
-# # List account activities (updates every 24 hours + 1 day delay)
-# res2 = snaptrade.account_information.get_account_activities(
-#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c",
-#     limit=2000,
-#     start_date=start_date,
-#     end_date=end_date,
-#     type="BUY,SELL,INTEREST,DIVIDEND,WITHDRAWAL,REI,STOCK_DIVIDEND,FEE,TAX,TRANSFER,SPLIT",
-# )
-# with open("activities.json", "w", encoding="utf-8") as f:
-#     json.dump(res2.body, f, indent=2, default=str)
+# List account activities (updates every 24 hours + 1 day delay)
+res2 = snaptrade.account_information.get_account_activities(
+    account_id="12740fda-39c7-4199-b569-c3f8ac982a6c",
+    limit=3000,
+    start_date=start_date,
+    end_date=end_date,
+    type="BUY,SELL,INTEREST,DIVIDEND,WITHDRAWAL,REI,STOCK_DIVIDEND,FEE,TAX,TRANSFER,SPLIT",
+)
+with open("activities.json", "w", encoding="utf-8") as f:
+    json.dump(res2.body, f, indent=2, default=str)
 
 # # List all account positions
 # res3 = snaptrade.account_information.get_all_account_positions(
@@ -42,14 +42,14 @@ snaptrade = SnapTrade(
 
 # # List all orders - last 24 hours
 # res4 = snaptrade.account_information.get_user_account_recent_orders(
-#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c"
+#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c", only_executed=True
 # )
 # with open("24hr_orders.json", "w", encoding="utf-8") as f:
 #     json.dump(res4.body, f, indent=2, default=str)
 
-# List all orders - past (30) days
-res5 = snaptrade.account_information.get_user_account_orders(
-    account_id="12740fda-39c7-4199-b569-c3f8ac982a6c", days=30
-)
-with open("all_orders.json", "w", encoding="utf-8") as f:
-    json.dump(res5.body, f, indent=2, default=str)
+# # List all orders - past (30) days
+# res5 = snaptrade.account_information.get_user_account_orders(
+#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c", days=30, state="executed"
+# )
+# with open("all_orders.json", "w", encoding="utf-8") as f:
+#     json.dump(res5.body, f, indent=2, default=str)
