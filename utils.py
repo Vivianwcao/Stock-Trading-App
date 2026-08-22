9b5a1c4552d74236a54b1f2c8e8718ca8af0fa0c
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-22 15:44:49 -0700

    test endponits

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
 import json
+from pprint import pprint
+
+with open("accounts.json", "r", encoding="utf-8") as f:
+    data_list = json.load(f)
+
+accounts_mapper = {
+    x["number"]: {
+        "account_id": x["id"],
+        "type": x["name"],
+        "balance": x["balance"]["total"]["amount"],
+        "first_transaction_date": x["sync_status"]["transactions"][
+            "first_transaction_date"
+        ],
+    }
+    for x in data_list
+    if x["meta"]["status"] == "open"
+}
+
+pprint(accounts_mapper)
