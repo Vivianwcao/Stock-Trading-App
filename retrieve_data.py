b0176b041fade779ac7cbfea095d91ca9019dda9
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-23 10:52:29 -0700

    implement date helper functions

--- retrieve_data.py
+++ retrieve_data.py
@@ -28,35 +28,44 @@ def get_activities(account_id, start_date=None) -> list[dict]:
     # bulk
     arguments = {
         "account_id": account_id,
-        "limit": 3000,
-        "type": "BUY,SELL,INTEREST,DIVIDEND,WITHDRAWAL,REI,STOCK_DIVIDEND,FEE,TAX,TRANSFER,SPLIT",
+        "limit": 1000,
+        "type": "BUY,SELL,INTEREST,DIVIDEND,WITHDRAWAL,REI,STOCK_DIVIDEND,FEE,TAX,TRANSFER,SPLIT,SUBSTITUTE_DIVIDEND,CONTRIBUTION",
         # start_date is defalted to the first transaction based on trade_date.
         # end_date is defalted to the last transaction based on trade_date.
     }
     if start_date:
-        #  from last certain (2) days (updates every 24 hours + 1 day delay)
+        #  from last certain (eg. 2) days (updates every 24 hours + 1 day delay)
         arguments["start_date"] = start_date
     res = snaptrade.account_information.get_account_activities(**arguments)
     return res.body["data"]
 
 
+# arguments = {
+#     "account_id": "12740fda-39c7-4199-b569-c3f8ac982a6c",
+#     "limit": 1000,
+#     "type": "BUY,SELL,INTEREST,DIVIDEND,WITHDRAWAL,REI,STOCK_DIVIDEND,FEE,TAX,TRANSFER,SPLIT,SUBSTITUTE_DIVIDEND,CONTRIBUTION",
+#     "start_date": "2026-08-20",
+# }
+# res = snaptrade.account_information.get_account_activities(**arguments)
+# print(res.body["data"])
+
 # # List all account positions
 # res3 = snaptrade.account_information.get_all_account_positions(
-#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c"
+#     account_id="*******"
 # )
 # with open("./test/positions.json", "w", encoding="utf-8") as f:
 #     json.dump(res3.body, f, indent=2, default=str)
 
 # # List all orders - last 24 hours
 # res4 = snaptrade.account_information.get_user_account_recent_orders(
-#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c", only_executed=True
+#     account_id="*******", only_executed=True
 # )
 # with open("./test/24hr_orders.json", "w", encoding="utf-8") as f:
 #     json.dump(res4.body, f, indent=2, default=str)
 
 # # List all orders - past (30) days
 # res5 = snaptrade.account_information.get_user_account_orders(
-#     account_id="12740fda-39c7-4199-b569-c3f8ac982a6c", days=30, state="executed"
+#     account_id="*******", days=30, state="executed"
 # )
 # with open("./test/all_orders.json", "w", encoding="utf-8") as f:
 #     json.dump(res5.body, f, indent=2, default=str)
