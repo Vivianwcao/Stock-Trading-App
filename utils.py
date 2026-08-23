e55fffd3644c4cca0a4addc297c40a75437674e2
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-23 09:22:03 -0700

    update api retrievers

--- utils.py
+++ utils.py
@@ -1,20 +1,25 @@
 import json
 from pprint import pprint
 
-with open("accounts.json", "r", encoding="utf-8") as f:
-    data_list = json.load(f)
 
-accounts_mapper = {
-    x["number"]: {
-        "account_id": x["id"],
-        "type": x["name"],
-        "balance": x["balance"]["total"]["amount"],
-        "first_transaction_date": x["sync_status"]["transactions"][
-            "first_transaction_date"
-        ],
+def create_accounts_mapper():
+    with open("./test/accounts.json", "r", encoding="utf-8") as f:
+        data_list = json.load(f)
+
+    accounts_mapper = {
+        x["number"]: {
+            "account_id": x["id"],
+            "type": x["name"],
+            "balance": x["balance"]["total"]["amount"],
+            "first_transaction_date": x["sync_status"]["transactions"][
+                "first_transaction_date"
+            ],
+        }
+        for x in data_list
+        if x["meta"]["status"] == "open" and x["balance"]["total"]["amount"] > 10
     }
-    for x in data_list
-    if x["meta"]["status"] == "open"
-}
 
-pprint(accounts_mapper)
+    with open("accounts_mapper.json", "w", encoding="utf-8") as f:
+        json.dump(accounts_mapper, f, indent=2, default=str)
+
+    print("Accounts mapper updated and saved.")
