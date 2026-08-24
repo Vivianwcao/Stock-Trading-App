f2a6d140f143a6f196b0e7c46bcfdb8fed09dd09
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-24 09:45:33 -0700

    implement retrieve recent orders

--- write_data.py
+++ write_data.py
@@ -26,12 +26,24 @@ def get_insert_query(table_name, source):
         """
 
 
-# Snaptrade activities only includes buy, sell and dividend
 # Snaptrade orders only includes buy and sell.
 TRANSPORT_TYPES = (
     "BUY",
     "SELL",
     "DIVIDEND",
+    "SUBSTITUTE_DIVIDEND",
+    "CONTRIBUTION",
+    "WITHDRAWAL",
+    "REI",
+    "STOCK_DIVIDEND",
+    "INTEREST",
+    "FEE",
+    "TAX",
+    "OPTIONEXPIRATION",
+    "OPTIONASSIGNMENT",
+    "OPTIONEXERCISE",
+    "TRANSFER",
+    "SPLIT",
 )
 
 
@@ -74,85 +86,47 @@ def init_db(conn):
     """)
 
 
-def update_activities(conn, is_bulk=False, is_from_orders=True):
-    if not is_from_orders:
-        get_accounts()
-        create_accounts_mapper()
+def erase_manual_activities(conn):
+    # keep only non buy/sell activities with the same amount
+    # remove all  buy or sell activities
+    conn.execute("""
+        delete from manual_activities
+        where type in ('BUY', 'SELL');
+    """)
 
-    with open("accounts_mapper.json", "r", encoding="utf-8") as f:
-        mapper = json.load(f)
 
+def fetch_activities(conn, accounts_mapper, is_bulk):
     all_records = []
+    start_date = None
 
-    # from API - activities
-    if not is_from_orders:
-        for account in mapper.items():
-            account_id = account["account_id"]
-
-            # find the latest transaction_date obtained from API
-            with conn.execute(
-                """
-                select 
-                    max(trade_date) latest_date
-                from activities
-                where account_id = ?
-            """,
-                (account_id,),
-            ) as cursor:
-                latest_transaction_date = cursor.fetchone()["latest_date"]
-
-            start_date = (
-                None
-                if is_bulk
-                else (to_api_date(latest_transaction_date) or x_days_ago(2))
-            )
-            # API fetch activities per WS account
-            try:
-                activities_list = get_activities(
-                    account_id, ",".join(TRANSPORT_TYPES), start_date=start_date
-                )
-
-            except Exception as e:
-                print(
-                    f"API fetching activities on account: {account_id} failed. Error: {e}"
-                )
-                return
-
-            for activity in activities_list:
-                all_records.append(
-                    (
-                        activity["id"],
-                        account_id,
-                        account["type"],
-                        activity["symbol"]["symbol"],
-                        activity["type"],
-                        activity["price"],
-                        activity["units"],
-                        activity["amount"],
-                        activity["fee"],
-                        activity["currency"]["code"],
-                        activity["trade_date"],
-                    )
-                )
-
-        with conn:
-            conn.executemany(
-                get_insert_query("activities", "api"),
-                all_records,
-            )
-        print("inserted all records successfully")
-
-    # from orders (real time update)
-
-    for account in mapper.items():
+    for account in accounts_mapper.items():
         account_id = account["account_id"]
 
-        # API fetch orders per WS account
+        # find the latest transaction_date obtained from API
+        with conn.execute(
+            """
+            select 
+                max(trade_date) latest_date
+            from activities
+            where account_id = ?
+        """,
+            (account_id,),
+        ) as cursor:
+            latest_transaction_date = cursor.fetchone()["latest_date"]
+
+        start_date = (
+            None if is_bulk else (to_api_date(latest_transaction_date) or x_days_ago(2))
+        )
+        # API fetch activities per WS account
         try:
-            activities_list = get_orders_last_24hrs(account_id)
+            activities_list = get_activities(
+                account_id, ",".join(TRANSPORT_TYPES), start_date=start_date
+            )
 
         except Exception as e:
-            print(f"API fetching orders on account: {account_id} failed. Error: {e}")
+            print(
+                f"API fetching activities on account: {account_id} failed. Error: {e}"
+            )
             return
 
         for activity in activities_list:
@@ -172,12 +146,79 @@ def update_activities(conn, is_bulk=False, is_from_orders=True):
                 )
             )
 
-    # keep only non buy/sell activities with the same amount
-    # remove all  buy or sell activities
-    conn.execute("""
-        delete from manual_activities
-        where type in ('BUY', 'SELL');
-    """)
+    with conn:
+        conn.executemany(
+            get_insert_query("activities", "api_activities"),
+            all_records,
+        )
+    print(f"inserted all activities records from {start_date} successfully")
+
+
+# update activities with recent orders
+def fetch_recent_orders(conn, accounts_mapper):
+    all_records = []
+
+    # from orders (real time update)
+    for account in accounts_mapper.items():
+        account_id = account["account_id"]
+
+        # API fetch orders per WS account
+        try:
+            orders_list = get_orders_last_24hrs(account_id)
+
+        except Exception as e:
+            print(
+                f"API fetching recent orders on account: {account_id} failed. Error: {e}"
+            )
+            return
+
+        for order in orders_list:
+            price = int(order["execution_price"])
+            qty = int(order["filled_quantity"])
+            all_records.append(
+                (
+                    order["brokerage_order_id"],
+                    account_id,
+                    account["type"],
+                    order["universal_symbol"]["symbol"],
+                    order["action"],
+                    price,
+                    qty,
+                    price * qty,
+                    0,
+                    order["universal_symbol"]["currency"]["code"],
+                    order["time_executed"],
+                )
+            )
+
+    with conn:
+        conn.executemany(
+            get_insert_query("activities", "api_orders"),
+            all_records,
+        )
+    print("inserted all order records from last 24 hours successfully")
+
+
+def daily_update(conn):
+    get_accounts()
+    create_accounts_mapper()
+
+    with open("accounts_mapper.json", "r", encoding="utf-8") as f:
+        mapper = json.load(f)
+
+    fetch_activities(conn, mapper, is_bulk=False)
+    fetch_recent_orders(conn, mapper)
+
+    erase_manual_activities(conn)
+
+
+def update_with_orders(conn):
+    with open("accounts_mapper.json", "r", encoding="utf-8") as f:
+        mapper = json.load(f)
+
+    fetch_recent_orders(conn, mapper)
+
+    erase_manual_activities(conn)
 
 
 if __name__ == "__main__":
