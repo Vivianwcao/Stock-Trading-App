1cb625c3bda6e300b56f08ecec44bac6c9138de7
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-25 09:45:31 -0700

    add accounts table

--- update_activities.py
+++ update_activities.py
@@ -22,13 +22,11 @@ def x_days_ago(x):
     return (datetime.now(timezone.utc).date() - timedelta(days=x)).isoformat()
 
 
-def get_insert_query(table_name, source):
-    return f"""
-            insert into {table_name} (
-                id, account_id, account_name, symbol, type, price, 
-                units, amount, fee, currency, trade_date, {source}
-            ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            on conflict(id, trade_date, account_id, symbol, type, price, units) do nothing;
+insert_activities_query = """
+            insert or ignore into activities (
+                id, account_id, symbol, type, price, 
+                units, amount, fee, currency, trade_date, source
+            ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
         """
 
 
@@ -56,10 +54,19 @@ TRANSPORT_TYPES = (
 # one time
 def init_db(conn):
     conn.executescript("""
+        create table if not exists accounts (
+            id text primary key, --snaptrade account_id
+            account_name text not null, --tfsa-absvdfh
+            account_type text not null,
+            status text,
+            first_transaction_date text,
+            institution text,
+            currency text default 'CAD',
+            last_successful_sync text not null -- utc timestamp from api
+        );
         create table if not exists activities (
             id text primary key,
             account_id text not null,
-            account_name text not null,
             symbol text not null,
             type text not null,
             price real,
@@ -70,9 +77,42 @@ def init_db(conn):
             trade_date text not null,
             source text not null,
 
+            foreign key (account_id)
+                references acounts(id)
             unique (trade_date, account_id, symbol, type, price, units)
         );
     """)
+
+
+def update_accounts(conn):
+    accounts_list = get_accounts(snaptrade)
+    records = [
+        (
+            account["id"],
+            account["number"],
+            account["meta"]["type"],
+            account["meta"]["status"],
+            account["sync_status"]["transactions"]["first_transaction_date"],
+            account["institution_name"],
+            account["meta"]["currency"],
+            account["sync_status"]["holdings"]["last_successful_sync"],
+        )
+        for account in accounts_list
+    ]
+    with conn:
+        conn.executemany(
+            """
+            insert into accounts (
+            id, account_name, account_type, status, first_transaction_date,
+            institution, currency, last_successful_sync)
+            values(?, ?, ?, ?, ?, ?, ?, ?)
+            on conflict(id) do update set
+                status = excluded.status,
+                last_successful_sync = excluded.last_successful_sync;
+        """,
+            records,
+        )
+    print("Updated accounts table successfully.")
 
 
 def fetch_activities(conn, accounts_mapper, is_bulk):
@@ -114,7 +154,6 @@ def fetch_activities(conn, accounts_mapper, is_bulk):
                 (
                     activity["id"],
                     account_id,
-                    account["type"],
                     activity["symbol"]["symbol"],
                     activity["type"],
                     activity["price"],
@@ -123,78 +162,56 @@ def fetch_activities(conn, accounts_mapper, is_bulk):
                     activity["fee"],
                     activity["currency"]["code"],
                     activity["trade_date"],
+                    "api_activities",
                 )
             )
 
     with conn:
         conn.executemany(
-            get_insert_query("activities", "api_activities"),
+            insert_activities_query,
             all_records,
         )
     print(f"inserted all activities records from {start_date} successfully")
 
 
-# update activities with recent orders
-def fetch_recent_orders(conn, accounts_mapper):
+# update activities with recent orders (per WS account)
+def fetch_recent_orders(conn, account_id):
     all_records = []
 
     # from orders (real time update)
-    for account in accounts_mapper.items():
-        account_id = account["account_id"]
+    # API fetch orders per WS account
+    try:
+        orders_list = get_orders_last_24hrs(snaptrade, account_id)
 
-        # API fetch orders per WS account
-        try:
-            orders_list = get_orders_last_24hrs(snaptrade, account_id)
+    except Exception as e:
+        print(f"API fetching recent orders on account: {account_id} failed. Error: {e}")
+        return
 
-        except Exception as e:
-            print(
-                f"API fetching recent orders on account: {account_id} failed. Error: {e}"
+    for order in orders_list:
+        price = float(order["execution_price"])
+        qty = float(order["filled_quantity"])
+        all_records.append(
+            (
+                order["brokerage_order_id"],
+                account_id,
+                order["universal_symbol"]["symbol"],
+                order["action"],
+                price,
+                qty,
+                price * qty,
+                0,
+                order["universal_symbol"]["currency"]["code"],
+                order["time_executed"],
+                "api_orders",
             )
-            return
-
-        for order in orders_list:
-            price = float(order["execution_price"])
-            qty = float(order["filled_quantity"])
-            all_records.append(
-                (
-                    order["brokerage_order_id"],
-                    account_id,
-                    account["type"],
-                    order["universal_symbol"]["symbol"],
-                    order["action"],
-                    price,
-                    qty,
-                    price * qty,
-                    0,
-                    order["universal_symbol"]["currency"]["code"],
-                    order["time_executed"],
-                )
-            )
+        )
 
     with conn:
         conn.executemany(
-            get_insert_query("activities", "api_orders"),
+            insert_activities_query,
             all_records,
         )
     print("inserted all order records from last 24 hours successfully")
-
-
-def daily_update(conn):
-    get_accounts(snaptrade)
-    create_accounts_mapper()
-
-    with open("accounts_mapper.json", "r", encoding="utf-8") as f:
-        mapper = json.load(f)
-
-    fetch_activities(conn, mapper, is_bulk=False)
-    fetch_recent_orders(conn, mapper)
-
-
-def update_with_orders(conn):
-    with open("accounts_mapper.json", "r", encoding="utf-8") as f:
-        mapper = json.load(f)
-
-    fetch_recent_orders(conn, mapper)
 
 
 if __name__ == "__main__":
