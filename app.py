53b688e715221f2d40b7f7632c125be939b8782a
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-26 14:13:40 -0700

    implement last fetched table

--- app.py
+++ app.py
@@ -19,42 +19,65 @@ logging.basicConfig(level=logging.INFO)  # required for local
 
 
 # The * means everything after it must be passed as named arguments
-def calculate_wait_time(conn, *, is_activities, hours=0, minutes=0):
-    # all accounts
-    if is_activities:
-        row = conn.execute("""
-            select
-                max(fetched_at) 
-            from activities
-            where source = 'api_activities'
-        """).fetchone()
-    else:
-        row = conn.execute("""
+# only one of the three (hours, minutes, seconds) should be passed
+def calculate_wait_time(conn, *, account_id=None, hours=0, minutes=0, seconds=0):
+    if account_id is None:
+        # activities, check all accounts
+        row = conn.execute(
+            """
             select
                 max(fetched_at)
-            from activities
-        """).fetchone()
+            from last_fetched
+            where source = 'activities'
+        """
+        ).fetchone()
+    else:
+        # orders, check by account
+        row = conn.execute(
+            """
+            select
+                fetched_at
+            from last_fetched
+            where account_id = ?
+        """,
+            (account_id,),
+        )
 
     current_time = datetime.now(timezone.utc)
     last_fetch_time = datetime.fromisoformat(row["fetched_at"].replace("Z", "+00:00"))
     elapsed = current_time - last_fetch_time
 
-    seconds = hours * 3600 + minutes * 60 - int(elapsed.total_seconds())
-    if seconds <= 0:
+    total_seconds = hours * 3600 + minutes * 60 + seconds - int(elapsed.total_seconds())
+    if total_seconds <= 0:
         return (0, 0, 0)
-    hrs = seconds // 3600
-    mins = seconds % 3600 // 60
-    secs = seconds % 60
+    hrs = total_seconds // 3600
+    mins = total_seconds % 3600 // 60
+    secs = total_seconds % 3600 % 60
     return hrs, mins, secs
 
 
 def click_update_all_activities(snaptrade, conn, hours=4):
-    hrs, mins, secs = calculate_wait_time(conn, is_activities=True, hours=hours)
+    hrs, mins, secs = calculate_wait_time(conn, hours=hours)
     if hrs == mins == secs == 0:
         # ready tp update:
         update_accounts(snaptrade, conn)
         time.sleep(30)
         fetch_activities(snaptrade, conn)
+    else:
+        logger.info(
+            "Too early to update. Still need to wait for %d hours %d minutes %d seconds.",
+            hrs,
+            mins,
+            secs,
+        )
+        return hrs, mins, secs
+
+
+def click_update_order_by_account(snaptrade, conn, account_id, seconds=30):
+    hrs, mins, secs = calculate_wait_time(conn, seconds=seconds)
+    if hrs == mins == secs == 0:
+        # ready tp update:
+        fetch_recent_orders(snaptrade, conn, account_id)
     else:
         logger.info(
             "Too early to update. Still need to wait for %d hours %d minutes %d seconds.",
