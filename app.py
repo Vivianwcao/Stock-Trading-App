3ba8d9a5fdb093fc27e486208acc2d9730ce34c4
Vivian Cao <vivian.w.cao@gmail.com>
2026-08-27 07:18:08 -0700

    add proper returns for HTTP calls

--- app.py
+++ app.py
@@ -4,56 +4,25 @@ import json
 from snaptrade import get_snaptrade_auth
 import logging
 import time
-from datetime import datetime, timedelta, timezone
 from update_activities import (
     init_db,
     update_accounts,
-    fetch_activities,
-    fetch_recent_orders,
+    update_activities,
+    update_recent_orders,
 )
+from utils import calculate_wait_time
 
 # ── Logging ─────────────────────────────────────────────────────────────────
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)  # required for lambda
 logging.basicConfig(level=logging.INFO)  # required for local
 
-
-# The * means everything after it must be passed as named arguments
-# only one of the three (hours, minutes, seconds) should be passed
-def calculate_wait_time(conn, *, account_id=None, hours=0, minutes=0, seconds=0):
-    if account_id is None:
-        # activities, check all accounts
-        row = conn.execute(
-            """
-            select
-                max(fetched_at)
-            from last_fetched
-            where source = 'activities'
-        """
-        ).fetchone()
-    else:
-        # orders, check by account
-        row = conn.execute(
-            """
-            select
-                fetched_at
-            from last_fetched
-            where account_id = ?
-        """,
-            (account_id,),
-        )
-
-    current_time = datetime.now(timezone.utc)
-    last_fetch_time = datetime.fromisoformat(row["fetched_at"].replace("Z", "+00:00"))
-    elapsed = current_time - last_fetch_time
-
-    total_seconds = hours * 3600 + minutes * 60 + seconds - int(elapsed.total_seconds())
-    if total_seconds <= 0:
-        return (0, 0, 0)
-    hrs = total_seconds // 3600
-    mins = total_seconds % 3600 // 60
-    secs = total_seconds % 3600 % 60
-    return hrs, mins, secs
+HEADERS = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",  # Allows Netlify frontend to fetch data
+    "Access-Control-Allow-Headers": "Content-Type,Authorization",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
 
 
 def click_update_all_activities(snaptrade, conn, hours=4):
@@ -61,47 +30,69 @@ def click_update_all_activities(snaptrade, conn, hours=4):
     if hrs == mins == secs == 0:
         # ready tp update:
         update_accounts(snaptrade, conn)
-        time.sleep(30)
-        fetch_activities(snaptrade, conn)
-    else:
-        logger.info(
-            "Too early to update. Still need to wait for %d hours %d minutes %d seconds.",
-            hrs,
-            mins,
-            secs,
-        )
-        return hrs, mins, secs
+        time.sleep(10)
+        update_activities(snaptrade, conn)
+        return {"status": "success", "message": "Updated all activities"}
+    return {
+        "status": "cooldown",
+        "wait": {"hours": hrs, "minutes": mins, "seconds": secs},
+    }
 
 
-def click_update_order_by_account(snaptrade, conn, account_id, seconds=30):
+def click_update_orders_by_account(snaptrade, conn, account_id, seconds=30):
     hrs, mins, secs = calculate_wait_time(conn, seconds=seconds)
     if hrs == mins == secs == 0:
         # ready tp update:
-        fetch_recent_orders(snaptrade, conn, account_id)
-    else:
-        logger.info(
-            "Too early to update. Still need to wait for %d hours %d minutes %d seconds.",
-            hrs,
-            mins,
-            secs,
-        )
-        return hrs, mins, secs
+        update_recent_orders(snaptrade, conn, account_id)
+        return {
+            "status": "success",
+            "message": f"Updated orders for account {account_id}",
+        }
+    return {
+        "status": "cooldown",
+        "wait": {"hours": hrs, "minutes": mins, "seconds": secs},
+    }
+
+
+def get_all_accounts(conn):
+    rows = conn.execute("select * from accounts").fetchall()
+    return [dict(row) for row in rows]
 
 
 def handler(event, context):
-    logger.info(json.dumps(event))
-
-    snaptrade = get_snaptrade_auth()
-    # 1. Connect to local database file (creates portfolio.db automatically)
-    conn = sqlite3.connect("stocks.db")
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON")
-
     try:
-        # conn.executescript("drop table activities")
-        init_db(conn)
-    finally:
-        conn.close()
+        logger.info(json.dumps(event))
+
+        action = event.get("action")
+        data = event.get("data")
+
+        snaptrade = get_snaptrade_auth()
+        # 1. Connect to local database file (creates portfolio.db automatically)
+        conn = sqlite3.connect("stocks.db")
+        conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA foreign_keys = ON")
+
+        try:
+            conn.executescript(
+                "drop table activities; drop table accounts; drop table last_fetched;"
+            )
+            init_db(conn)  # run once
+            if action == "update_all_activities":
+                res = click_update_all_activities(snaptrade, conn, hours=4)
+            elif action == "update_orders_by_account":
+                res = click_update_orders_by_account(
+                    snaptrade, conn, account_id=data, seconds=30
+                )
+            elif action == "get_all_account":
+                res = get_all_accounts(conn)
+            else:
+                return {"statusCode": 400, "headers": HEADERS, "body": "Invalid action"}
+            return {"statusCode": 200, "headers": HEADERS, "body": json.dumps(res)}
+        finally:
+            conn.close()
+    except Exception as e:
+        logger.exception("Request failed.")
+        return {"statusCode": 500, "headers": HEADERS, "body": str(e)}
 
 
 if __name__ == "__main__":
