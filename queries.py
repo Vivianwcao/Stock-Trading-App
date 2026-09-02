ea647e8a5579200b036dc88b16ac7096cf1a449d
Vivian Cao <vivian.w.cao@gmail.com>
2026-09-02 12:31:37 -0700

    add queries

--- queries.py
+++ queries.py
@@ -1,4 +1,5 @@
 from utils import to_dicts, to_dict
+from update_tables import TRANSPORT_TYPES
 
 
 def get_all_active_accounts(client):
@@ -9,3 +10,28 @@ def get_all_active_accounts(client):
             and balance > 10
         """)
     return to_dicts(res)
+
+
+def get_activities_single_account(
+    client, account_id, symbol, start_date, end_date, types=TRANSPORT_TYPES
+):
+    client.execute(
+        """
+        select
+            trade_date,
+            type,
+            price,
+            units,
+            amount,
+
+        from activities
+        where 
+            account_id = ?
+            and (symbol is null or symbol = ?)
+            and types in ?
+            and trade_date >= ?
+            and trade_date <= ?
+        order by datetime(trade_date)
+        """,
+        (),
+    )
