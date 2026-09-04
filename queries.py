2de090d54bc22180aa98f5f17018aa46f2761ea1
Vivian Cao <vivian.w.cao@gmail.com>
2026-09-04 15:40:19 -0700

    update main query

--- queries.py
+++ queries.py
@@ -26,7 +26,7 @@ def get_trading_activities_single_account(
             type,
             price,
             units,
-            amount amount,
+            amount,
             -- for reset cycles
             sum(units) over(partition by symbol order by trade_date) rolling_units
         from activities
@@ -39,6 +39,7 @@ def get_trading_activities_single_account(
         with_pres as (
         SELECT
             *,
+            lag(type) over(partition by symbol order by trade_date) pre_type,
             (
             SELECT
                 type
@@ -48,8 +49,7 @@ def get_trading_activities_single_account(
                 and type <> 'DIVIDEND'
             order by trade_date DESC
             limit 1
-            ) pre_type,
-            -- lag(type) over(partition by symbol order by trade_date) pre_type,
+            ) pre_trade_type,
             lag(rolling_units) over(partition by symbol order by trade_date) pre_rolling_units
         from cleaned c
         ),
@@ -57,14 +57,14 @@ def get_trading_activities_single_account(
         SELECT
             *,
             count(*) filter(where 
-            (pre_type = 'SELL' and type = 'BUY' and pre_rolling_units<=0)) 
+            (pre_trade_type = 'SELL' and type = 'BUY' and pre_rolling_units<=0)) 
             over (partition by symbol order by trade_date) reset
         FROM with_pres
         ),
         sell_grouped as (
         select
             *,
-            count(*) filter(where type='SELL') over(partition by symbol, reset order by trade_date) sell_reset
+            count(*) filter(where pre_type='SELL') over(partition by symbol, reset order by trade_date) sell_reset
         from sell_all_grouped
         ),
         partitioned as (
@@ -75,13 +75,22 @@ def get_trading_activities_single_account(
             round(sum(amount) filter(where type = 'BUY') over(partition by symbol, reset order by trade_date), 2) bought_balance,
             round(sum(amount) filter(where type = 'BUY') over(partition by symbol, reset order by trade_date)
             /sum(units) filter(where type = 'BUY') over(partition by symbol, reset order by trade_date), 3) avg_bought_price,
-            sum(units) filter(where type = 'SELL') over(partition by symbol, reset order by trade_date) sold_units,
-            round(sum(amount) filter(where type = 'SELL') over(partition by symbol, reset order by trade_date), 2) sold_balance,
             sum(amount) filter(where type = 'DIVIDEND') over(partition by symbol, reset, sell_reset order by trade_date) divident_balance
         FROM sell_grouped
         )
         SELECT
-        *
+        account_id,
+        trade_date,
+        symbol,
+        type,
+        price,
+        units,
+        amount,
+        reset,
+        sell_reset,
+        current_balance,
+        avg_bought_price,
+        divident_balance
         FROM partitioned;
         """,
         (),
