e433ab78fe103b58cf709002c35002cd0e2882e6
Vivian Cao <vivian.w.cao@gmail.com>
2026-09-17 13:09:15 -0700

    implement get_ratios_from_positions queries

--- queries.sql
+++ queries.sql
@@ -272,36 +272,67 @@ on act.account_id = acc.id
 group by nickname;
 
 
-with totals as (
-    select 
-        account_id,
-        sum(holdings * cost_basis) total_bought, 
-        sum(holdings * current_price) total_current
-    from positions
-    group by account_id
+with latest_date as(
+	select 
+		nickname,
+		symbol,
+		max(trade_date) latest_date
+	from transactions
+	where symbol is not null
+	group by 
+		nickname,
+		symbol
+),
+dividends as (
+	select 
+		nickname,
+		symbol,
+		max(dividend_balance) dividend_balance
+	from transactions
+	where (nickname, symbol, trade_date) in (
+		select 
+			nickname,
+			symbol,
+			latest_date 
+		from latest_date
+	)
+	group by 
+		nickname,
+		symbol 
+),
+totals as (
+	select 
+		account_id,
+		sum(holdings * cost_basis) total_bought, 
+		sum(holdings * current_price) total_current
+	from positions
+	group by account_id
 )
 select
-    nickname, 
-    p.account_id,
-    symbol,
-    holdings,
-    cost_basis,
-    current_price,
-    round(holdings * cost_basis, 4) bought_balance,
-    round(total_bought, 4) total_bought,
-    round(holdings * cost_basis*100 / total_bought, 2) bought_percentage,
-    round((current_price - cost_basis)*100 / cost_basis, 2) growth_percentage,
-    round(holdings * current_price, 4) current_balance,
-    round(total_current, 4) total_current,
-    round(holdings * current_price*100 / total_current, 2) current_percentage,
-    rank() over(partition by account_id order by holdings * cost_basis*100 / total_bought desc) bought_percentage_rnk,
-    rank() over(partition by account_id order by holdings * current_price*100 / total_current desc) current_percentage_rnk,
-    rank() over(partition by account_id order by holdings * cost_basis desc) bought_balance_rnk,
-    rank() over(partition by account_id order by holdings * current_price desc) current_balance_rnk,
-    rank() over(partition by account_id order by (current_price - cost_basis)*100 / cost_basis desc) growth_percentage_rnk,
-    p.last_successful_sync
+	nickname, 
+	p.account_id,
+	p.symbol,
+	holdings,
+	cost_basis,
+	current_price,
+	round((current_price - cost_basis)*100 / cost_basis, 2) growth_percentage,
+	round(holdings * cost_basis, 4) bought_balance,
+	round(total_bought, 4) total_bought,
+	round(holdings * cost_basis*100 / total_bought, 2) bought_ratio,
+	round(holdings * current_price, 4) current_balance,
+	round(total_current, 4) total_current,
+	round(holdings * current_price*100 / total_current, 2) current_ratio,
+	dividend_balance,
+	rank() over(partition by nickname order by holdings * cost_basis*100 / total_bought desc) bought_ratio_rnk,
+	rank() over(partition by nickname order by holdings * current_price*100 / total_current desc) current_ratio_rnk,
+	rank() over(partition by nickname order by holdings * cost_basis desc) bought_balance_rnk,
+	rank() over(partition by nickname order by holdings * current_price desc) current_balance_rnk,
+	rank() over(partition by nickname order by (current_price - cost_basis)*100 / cost_basis desc) growth_percentage_rnk,
+  p.last_successful_sync
 from positions p
-join totals
-    using(account_id)
+join totals t
+	using(account_id)
 join accounts
-    on p.account_id = id;
+	on p.account_id = id
+join dividends
+	using(nickname, symbol);
