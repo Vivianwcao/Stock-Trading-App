fadc0dab5aea30152d0686abc840e74ac6249cfc
Vivian Cao <vivian.w.cao@gmail.com>
2026-09-18 12:05:49 -0700

    alter transactions view

--- queries.py
+++ queries.py
@@ -192,10 +192,10 @@ def init_db(conn):
             rn,
             case when type = 'BUY' then amount
                 else 0
-            end bought_balance,
+            end cost,
             case when type = 'BUY' then price
                 else 0
-            end avg_bought_price
+            end avg_cost
             from partitioned
             -- seeds condition
             where rn = 1
@@ -213,14 +213,14 @@ def init_db(conn):
             p.cycles,
             p.holdings_per_cycle,
             p.rn,
-            case when p.type = 'BUY' then t.bought_balance + p.amount
-            when p.type = 'SELL' then t.bought_balance - t.avg_bought_price * p.units
-            else t.bought_balance
-            end as bought_balance,
+            case when p.type = 'BUY' then t.cost + p.amount
+            when p.type = 'SELL' then t.cost - t.avg_cost * p.units
+            else t.cost
+            end as cost,
 
-            case when p.type = 'BUY' then abs(coalesce((t.bought_balance + p.amount)/nullif(p.holdings_per_cycle, 0), p.price))
-            else t.avg_bought_price
-            end as avg_bought_price
+            case when p.type = 'BUY' then abs(coalesce((t.cost + p.amount)/nullif(p.holdings_per_cycle, 0), p.price))
+            else t.avg_cost
+            end as avg_cost
             from partitioned p
             join tree t 
             on p.nickname = t.nickname
@@ -241,16 +241,16 @@ def init_db(conn):
             t.cycles,
             t.holdings_per_cycle,
             round(p.dividend_balance, 4) AS dividend_balance,
-            round(bought_balance, 4) AS bought_balance,
-            round(avg_bought_price, 4) AS avg_bought_price,
+            round(cost, 4) AS cost,
+            round(avg_cost, 4) AS avg_cost,
             CASE
             WHEN t.type = 'SELL' THEN round(
-                (t.price - avg_bought_price) * 100 / nullif(avg_bought_price, 0),
+                (t.price - avg_cost) * 100 / nullif(avg_cost, 0),
                 2
             )
             END AS return_percentage,
             CASE
-            WHEN t.type = 'SELL' THEN round((t.price - avg_bought_price) * abs(t.units), 2)
+            WHEN t.type = 'SELL' THEN round((t.price - avg_cost) * abs(t.units), 2)
             END AS realized_profit
         FROM tree t
         join partitioned p
